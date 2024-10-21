235fb05abe4a947defff2ce0bc2831da057d34e1
Kanagawa Yamada <83739103+LoggingNewMemory@users.noreply.github.com>
2024-10-21 14:38:40 +0700

    Add files via upload

---
+++ AyundaRisu/AyundaRisu.sh
@@ -0,0 +1,7 @@
+service call SurfaceFlinger 1006 i64 1 i64 1
+service call SurfaceFlinger 1008 i32 9
+service call SurfaceFlinger 10  08 i32 1
+service call SurfaceFlinger 1036 i32 0
+service call SurfaceFlinger 1023 i32 0
+service call SurfaceFlinger 1022 f 5.5
+service call SurfaceFlinger 1035 i32 0
---
+++ customize.sh
@@ -0,0 +1,47 @@
+LATESTARTSERVICE=true
+
+ui_print "🗡--------------------------------🗡"
+ui_print "      Ayunda Risu Fake AMOLED       "
+ui_print "🗡--------------------------------🗡"
+ui_print "         By: Kanagawa Yamada        "
+ui_print "------------------------------------"
+ui_print "      READ THE TELEGRAM MESSAGE     "
+ui_print "------------------------------------"
+ui_print " "
+sleep 1.5
+
+ui_print "-----------------📱-----------------"
+ui_print "            DEVICE INFO             "
+ui_print "-----------------📱-----------------"
+ui_print "DEVICE : $(getprop ro.build.product) "
+ui_print "MODEL : $(getprop ro.product.model) "
+ui_print "MANUFACTURE : $(getprop ro.product.system.manufacturer) "
+ui_print "PROC : $(getprop ro.product.board) "
+ui_print "CPU : $(getprop ro.hardware) "
+ui_print "ANDROID VER : $(getprop ro.build.version.release) "
+ui_print "KERNEL : $(uname -r) "
+ui_print "RAM : $(free | grep Mem |  awk '{print $2}') "
+ui_print " "
+sleep 1.5
+
+ui_print "-----------------🗡-----------------"
+ui_print "            MODULE INFO             "
+ui_print "-----------------🗡-----------------"
+ui_print "Name : Ayunda Risu Fake AMOLED"
+ui_print "Version : V 1.0"
+ui_print "Support Root : Magisk / KernelSU"
+ui_print " "
+sleep 1.5
+
+ui_print "      DON'T BLAME ME IF YOUR        "
+ui_print "         SCREEN GETS BLACK          "
+ui_print " "
+sleep 3
+
+ui_print "      Installing Ayunda Risu        "
+sleep 3
+
+unzip -o "$ZIPFILE" 'AyundaRisu/*' -d $MODPATH >&2
+set_perm_recursive $MODPATH/AyundaRisu 0 0 0755 0644
+
+am start -a android.intent.action.VIEW -d https://t.me/KanagawaLabAnnouncement/143 >/dev/null 2>&1
