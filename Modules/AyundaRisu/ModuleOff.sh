cbac710af6db46069cfe5cfe0ceb083ceac1e4b5
LoggingNewMemory <albert.wesley.dion@gmail.com>
2025-08-19 19:58:15 +0700

    drop all uneeded surface flinger

--- Modules/AyundaRisu/ModuleOff.sh
+++ Modules/AyundaRisu/ModuleOff.sh
@@ -1,6 +1,1 @@
-service call SurfaceFlinger 1006 i64 0 i64 0
-service call SurfaceFlinger 1008 i32 0
-service call SurfaceFlinger 1008 i32 0
-service call SurfaceFlinger 1036 i32 1
-service call SurfaceFlinger 1023 i32 1
 service call SurfaceFlinger 1022 f 1.0
